f4f7084914fc69aac193e962fe96a44f8b84f906
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-01-05 09:32:32 +0100

    commit changes in html, css and script file

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ const navBar = document.querySelector(".nav-bar");
 btnEl.addEventListener("click", function () {
   navBar.classList.toggle("open-nav");
 });
+
+/*this selects the anchor links prop with*/
+const linksAll = document.querySelectorAll("a:link");
+
+/*--forEach for adding multiple links to the eventlistner++*/
+
+linksAll.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    //e.preventDefault();
+
+    //Read the href attribute from the link to scroll to coressponding link//
+
+    //const href = link.getAttribute("href");
+
+    /* if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }*/
+
+    //closing mobile navigation
+    if (link.classList.contains("nav-links"))
+      navBar.classList.toggle("open-nav");
+  });
+});
+
+//https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
