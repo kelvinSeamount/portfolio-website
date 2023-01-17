8df2a43b46a43751154972dc2bbc345603f349a0
KelvinSeamount <kelvinnwachukwu89@gmail.com>
2023-01-17 22:34:50 +0100

    aadded sticky navigation

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const btnHeader = document.querySelector(".header");
 btnNav.addEventListener("click", function () {
   btnHeader.classList.toggle("nav-open");
 });
+
+//Implementing--Sticky--Navigation--//
+const sectionHeroEl = document.querySelector(".hero-bg");
+
+const sTicky = new IntersectionObserver(
+  function (entries) {
+    const array = entries[0];
+    if (array.isIntersecting === false) {
+      document.body.classList.add("sticky-nav");
+    }
+
+    if (array.isIntersecting === true) {
+      document.body.classList.remove("sticky-nav");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-96px",
+  }
+);
+sTicky.observe(sectionHeroEl);
